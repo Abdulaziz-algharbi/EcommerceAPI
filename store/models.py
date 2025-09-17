54ee379159f67f049d2383ec9d496ecd9743e5e6
abdulaziz <abdulazizalgharbi8@gmail.com>
2025-09-17 10:37:49 +0400

    Build the initial Data Model of store and tag apps

--- store/models.py
+++ store/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
-# Create your models here.
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(
+        max_digits=6, decimal_places=2
+    )
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)
+    promotions = models.ManyToManyField(Promotion)
+    collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateField(null=True)
+
+class Address(models.Model):
+    customer = models.OneToOneField(
+        Customer,
+        on_delete=models.CASCADE, 
+        primary_key=True
+    )
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+
+class Order(models.Model):
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'F'
+    PAYMENT_STATUS_CHOICES = [
+        (PAYMENT_STATUS_PENDING, 'Pending'),
+        (PAYMENT_STATUS_COMPLETE, 'Complete'),
+        (PAYMENT_STATUS_FAILED, 'Failed')
+    ]
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(
+        max_length=1,
+        choices=PAYMENT_STATUS_CHOICES,
+        default=PAYMENT_STATUS_PENDING
+    )
